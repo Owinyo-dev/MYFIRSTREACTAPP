fc7e4bd6b74720a9cfb09821d36c3635d518dfc4
BrendaOwinyo <breapondi@gmail.com>
2025-07-16 21:31:19 +0300

    props

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,11 +1,12 @@
-import Card from "./card.jsx";
+import Student from "./student.jsx";
 
 function App() {
   return (
     <>
-      <Card />
-      <Card />
-      <Card />
+      <Student name="Spongebob" age={30} isStudent={true} />
+      <Student name="Patrick" age={42} isStudent={false} />
+      <Student name="Squidward" age={50} isStudent={false} />
+      <Student name="Sandy" age={27} isStudent={true} />
     </>
   );
 }
